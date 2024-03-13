85db88613c53d6c9c86eeb6a9f8a7768d47944d4
romanlatipov <romanlatipov@gmail.com>
2024-03-13 11:38:01 -0400

    complete first class functions lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(arr) {
+    const newArr = [arr[0], arr[1]];
+    return newArr;
+}
+function returnLastTwoDrivers(arr) {
+    const newArr = [arr[arr.length-2], arr[arr.length-1]];
+    return newArr;
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(i) {
+    const fareMultiplier = fare => fare * i;
+    return fareMultiplier;
+}
+function fareDoubler(fare) {
+    const newFare = createFareMultiplier(2)(fare);
+    return newFare;
+}
+function fareTripler(fare) {
+    const newFare = createFareMultiplier(3)(fare);
+    return newFare;
+}
+function selectDifferentDrivers(arrayOfDrivers, fun) {
+    return fun(arrayOfDrivers);
+}
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
